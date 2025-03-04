9a6bc92cc6f7da6fcd1f466035eeb9a051728822
Andrei Zhidenkov <pensnarik@gmail.com>
2025-03-04 14:20:05 +0300

    Add Container() class

---
+++ aa/container.py
@@ -0,0 +1,12 @@
+from aa.db import Database
+
+class Container():
+
+    def __init__(self, path: str):
+        self.path = path
+
+    def __get_mountpoint(self):
+
+
+    def save(self):
+        pass
